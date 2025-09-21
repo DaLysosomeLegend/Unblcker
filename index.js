5141aa692310bc9d82f9c2da8514339ece234856
DaLysosomeLegend <tejhas.email@gmail.com>
2025-09-21 09:56:37 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Unblocker = require('unblocker');
+const app = express();
+
+app.use(Unblocker({
+  prefix: '/proxy/',
+  requestMiddleware: [
+    function(req, res, next) {
+      console.log('Incoming cookies:', req.headers.cookie);
+      next();
+    }
+  ],
+  responseMiddleware: [
+    function(req, res, next) {
+      const setCookie = res.getHeader('Set-Cookie');
+      if (setCookie) {
+        console.log('Set-Cookie from target:', setCookie);
+      }
+      next();
+    }
+  ]
+}));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Proxy Unblocker</h1>
+    <form method="GET" action="/proxy/">
+      <input name="url" placeholder="https://example.com" style="width:300px;">
+      <button type="submit">Go</button>
+    </form>
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy running on port ${PORT}`);
+});
